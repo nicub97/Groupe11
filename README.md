5c067bdf84083522574f300f1da913d0bead655b
Nicu Balica <nbalica@myges.fr>
2025-06-13 10:08:28 +0100

    debug

--- README.md
+++
@@ -1,5 +0,0 @@
-# Groupe11
-Contributeurs
-Elève 1 = Alexandre
-Elève 2 = Denis
-Elève 3 = Nicuprotected ?
--- td 4 Tutunaru cristiana/obj/main.d
+++
@@ -1,7 +0,0 @@
-obj/main.o: src/main.c include/graphicSDL.h include/params.h \
- include/vacuumrobot.h include/matrice.h include/sacVecteurs.h
-include/graphicSDL.h:
-include/params.h:
-include/vacuumrobot.h:
-include/matrice.h:
-include/sacVecteurs.h:
