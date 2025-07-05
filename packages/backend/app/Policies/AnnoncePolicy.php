11b59591a4eaf49b1e93e1e37f10dc1c03805358
nicub97 <nbalica@myges.fr>
2025-07-05 10:29:26 +0100

    Add driver reservation check to pay policy

--- packages/backend/app/Policies/AnnoncePolicy.php
+++ packages/backend/app/Policies/AnnoncePolicy.php
@@ -12,10 +12,23 @@ class AnnoncePolicy
      */
     public function pay(Utilisateur $user, Annonce $annonce): bool
     {
-        return (
+        $authorized = (
             $annonce->id_client && $annonce->id_client === $user->id
         ) || (
             $annonce->id_commercant && $annonce->id_commercant === $user->id
         );
+
+        if (! $authorized) {
+            return false;
+        }
+
+        if (
+            $annonce->type === 'livraison_client' &&
+            $annonce->id_livreur_reservant === null
+        ) {
+            return false;
+        }
+
+        return true;
     }
 }
