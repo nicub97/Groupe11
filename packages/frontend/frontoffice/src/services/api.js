01b5dc4832dd032cbbf22af5b5ced84357579dd5
nicub97 <nbalica@myges.fr>
2025-07-12 10:00:29 +0100

    Update api.js

--- packages/frontend/frontoffice/src/services/api.js
+++ packages/frontend/frontoffice/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  VITE_API_URL=http://localhost:8000/api # TODO: changer cette URL avec celle de l'API en production
   withCredentials: true
 });
 
