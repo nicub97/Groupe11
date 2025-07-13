ebd2a3c056eb1ecfb13eb13c796df2a9dd7ae5d8
nicub97 <nbalica@myges.fr>
2025-07-13 20:59:08 +0100

    Revert "Fix Sanctum SPA session"

--- packages/backend/routes/web.php
+++ packages/backend/routes/web.php
@@ -1,14 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-// Routes d'authentification pour le mode SPA
-Route::middleware('web')->group(function () {
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 });
